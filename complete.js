beb0450da24ece68c685fcdb2fc1491df2475aed
TruongTX <me@truongtx.me>
2018-08-29 18:38:18 +0700

    Change to use js script

--- complete.js
+++ complete.js
@@ -32,23 +32,44 @@ if (fileCount === 1) {
   const command = `rclone copy "${filePath}" "${rcloneRemoteName}:${rcloneRemoteDir}"`;
   console.log('command:', command);
   childProcess.exec(command, {}, (err, stdout, stderr) => {
-    console.log('stdout:', stdout);
-    process.exit(0);
-
     if (err) {
       console.log('err:', err);
       console.log('stderr:', stderr);
       process.exit(1);
     }
+
+    process.exit(0);
+  });
+} else {
+  // multiple file, create directory on remote first
+
+  // get dir name first
+  let filePathNoDataDir = filePath.substring(dataDir.length);
+  if (filePathNoDataDir[0] === path.sep) {
+    filePathNoDataDir = filePathNoDataDir.substring(1);
+  }
+  const dirName = filePathNoDataDir.split(path.sep)[0];
+  const dirPath = path.join(dataDir, dirName);
+
+  const mkdirCommand = `rclone mkdir "${rcloneRemoteName}:${rcloneRemoteDir}/${dirName}"`;
+  console.log('mkdirCommand:', mkdirCommand);
+  childProcess.exec(mkdirCommand, {}, (err, stdout, stderr) => {
+    if (err) {
+      console.log('err:', err);
+      console.log('stderr:', stderr);
+      process.exit(1);
+    }
+
+    const copyCommand = `rclone copy "${dirPath}/" "${rcloneRemoteName}:${rcloneRemoteDir}/${dirName}/"`;
+    console.log('copyCommand', copyCommand);
+    childProcess.exec(copyCommand, {}, (err, stdout, stderr) => {
+      if (err) {
+        console.log('err:', err);
+        console.log('stderr:', stderr);
+        process.exit(1);
+      }
+
+      process.exit(0);
+    });
   });
 }
-
-// multiple file, create directory on remote first
-
-// get dir name first
-let filePathNoDataDir = filePath.substring(dataDir.length);
-if (filePathNoDataDir[0] === path.sep) {
-  filePathNoDataDir = filePathNoDataDir.substring(1);
-}
-const dirName = filePathNoDataDir.split(path.sep)[0];
-const dirPath = path.join(dataDir, dirName);
