665096a095ae5a677a2759640284ea1d3d92d8ff
TruongTX <me@truongtx.me>
2018-08-29 16:06:59 +0700

    Update complete.sh script

--- complete.sh
+++ complete.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 
+echo "GID: $1"
+echo "Number of files: $2"
+echo "File Path: $3"
+echo "Rclone Enabled: $RCLONE_ENABLED"
+echo "Remote name: $RCLONE_REMOTE_NAME"
+echo "Remote dir: $RCLONE_REMOTE_DIR"
+echo "rclone copy $3 $RCLONE_REMOTE_NAME:$RCLONE_REMOTE_DIR"
+
 if [ "$RCLONE_ENABLED" = "true" ]
 then
     rclone copy $3 $RCLONE_REMOTE_NAME:$RCLONE_REMOTE_DIR
